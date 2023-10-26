e109c2bf69e6bcd8711c164a968b3102591e7d7f
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-10-26 17:09:41 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,66 +1,66 @@
-
+#!/bin/bash
 
 # Define the path to your file
-$username = $env:USERNAME
-$basePath = "C:/Users/${username}/booktmp/"
+username=$USER
+basePath="/home/$username/booktmp/"
 
-$pathExists = (Test-Path -Path "C:/Users/${username}/booktmp/")
-if (!($pathExists)) {
-    New-Item -ItemType Directory -Path "C:/Users/${username}/booktmp/"
-}
+# Check if the path exists, and create it if it doesn't
+if [ ! -d "$basePath" ]; then
+    mkdir -p "$basePath"
+fi
 
+filePath="/home/$username/booktmp/Readfile.txt"
+registryPath="https://raw.githubusercontent.com/sartimo/read/main/Readfile"
 
-$filePath = "C:/Users/${username}/booktmp/Readfile.txt"
-$registryPath = "https://raw.githubusercontent.com/sartimo/read/main/Readfile"
+# Download the file from the registry
+curl -o "$filePath" "$registryPath"
+echo "Successfully fetched Latest Readfile from $registryPath."
+echo "Saved to $filePath"
 
-Invoke-WebRequest -Uri $registryPath -OutFile $filePath
-Write-Host "Successfully fetched Latest Readfile from ${registryPath}."
-Write-Host "Saved to ${filePath}"
-# Initialize an empty hashtable to store the links
-$links = @{}
+# Initialize an empty associative array to store the links
+declare -A links
 
 # Read the file line by line
-Get-Content -Path $filePath | ForEach-Object {
-    # Split each line on the ":" character
-    $parts = $_ -split ":", 2
+while IFS=: read -r linkName url; do
+    # Trim leading and trailing spaces from link name and URL
+    linkName="${linkName#"${linkName%%[![:space:]]*}"}"
+    linkName="${linkName%"${linkName##*[![:space:]]}"}"
+    url="${url#"${url%%[![:space:]]*}"}"
+    url="${url%"${url##*[![:space:]]}"}"
 
-    # Ensure the line contains both parts
-    if ($parts.Count -eq 2) {
-        # Trim leading and trailing spaces from link name and URL
-        $linkName = $parts[0].Trim()
-        $url = $parts[1].Trim()
-
-        # Store the link and URL in the hashtable
-        $links[$linkName] = $url
-    }
-}
+    # Store the link and URL in the associative array
+    links["$linkName"]="$url"
+done < "$filePath"
 
 # Display a list of all link names
-Write-Host "List of all available Books:"
-Write-Host ""
+echo "List of all available Books:"
+echo ""
 
-$links.Keys | ForEach-Object {
-    Write-Host $_
-}
+for linkName in "${!links[@]}"; do
+    echo "$linkName"
+done
 
-Write-Host ""
+echo ""
 
 # Prompt the user to choose a link
-$chosenLinkName = Read-Host "Enter the Book you want to read"
+read -p "Enter the Book you want to read: " chosenLinkName
 
-# Check if the chosen link name exists in the hashtable
-if ($links.ContainsKey($chosenLinkName)) {
+# Check if the chosen link name exists in the associative array
+if [ -v "links[$chosenLinkName]" ]; then
     # Store the chosen link in a variable
-    $chosenLink = $links[$chosenLinkName]
+    chosenLink="${links[$chosenLinkName]}"
 
     # Display the chosen link
-    Write-Host "Chosen Book: $chosenLink"
+    echo "Chosen Book: $chosenLink"
 
-    $linkExists = (Test-Path -Path "$basePath/$chosenLinkName.html")
-    if (!($pathExists)) {
-        Invoke-WebRequest -Uri $chosenLink -OutFile "$basePath/$chosenLinkName.html"
-
-    }
-} else {
-    Write-Host "Book not found."
-}
+    # Check if the file exists in the base path
+    if [ ! -f "$basePath/$chosenLinkName.html" ]; then
+        # Download the chosen link
+        curl -o "$basePath/$chosenLinkName.html" "$chosenLink"
+        html2text "$basePath/$chosenLinkName.html"  >> $basePath/$chosenLinkName.txt
+        rm $basePath/$chosenLinkName.html
+        nano $basePath/$chosenLinkName.txt
+    fi
+else
+    echo "Book not found."
+fi
