6e510e118e7c31be3a6014883f3246a903df55b8
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-10-26 15:49:39 +0200

    Update main.ps1

--- src/main.ps1
+++ src/main.ps1
@@ -1,3 +1,6 @@
+# requires PScore6
+iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Preview"
+
 readfileLocation = "%APPDATA%/Roadming/Readfile"
 readfileRegistry = "https://raw.githubusercontent.com/sartimo/read/main/Readfile"
 
@@ -5,5 +8,9 @@ if !(Test-Path -path $readfileLocation) {
     write-host "Readfile doesn't exist. Fetching latest Readfile from registry...\n"
     (New-Object System.Net.WebClient).DownloadFile(readfileRegistry, readfileLocation)  
 } else {
-    
+    # read Readfile
+    # choose book
+    # fetch book html
+    # parse html into markdown
+    # read ma
 }
