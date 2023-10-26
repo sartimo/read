54c36b4eb02dcb1b4f7841bf700e3e30e2e5c78a
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-10-26 14:47:19 +0200

    Update and rename cli.c to main.sh

---
+++ src/main.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Function to check if a dep is installed
+check_command() {
+    if command -v "$1" &>/dev/null; then
+        echo "$1 is installed."
+    else
+        echo "$1 is not installed but required"
+        while true; do
+            read -p "Do you want to install $1 (yes/no)? " choice
+            case "$choice" in
+                [Yy]*)
+                    sudo apt-get install -y "$1"
+                    echo "$1 is now installed."
+                    break
+                    ;;
+                [Nn]*)
+                    break
+                    ;;
+                *)
+                    echo "Please enter 'yes' or 'no'."
+                    ;;
+            esac
+        done
+    fi
+}
+
+check_command "curl"
+check_command "html2text"
+
+file_path="~/.librc"
+
+if [ -e "$file_path" ]; then
+    echo "Found lib config at: $file_path"
+else
+    echo "File $file_path does not exist"
+fi
